11332e31507dcef78d1dcc6832d178f52bdb7da2
MohamedMagdyOmar <eng_mohamed_magdy@hotmail.com>
2020-09-21 23:43:36 -0400

    Mocking Child Components

--- PSAngularUnitTestingCourse-master/src/app/heroes/heroes.component.shallow.spec.ts
+++ PSAngularUnitTestingCourse-master/src/app/heroes/heroes.component.shallow.spec.ts
@@ -4,11 +4,27 @@ import { NO_ERRORS_SCHEMA } from "@angular/core";
 import { HeroesComponent } from "./heroes.component";
 import { HeroService } from "../hero.service";
 import { of } from 'rxjs';
+import { Component, Input, Output, EventEmitter } from '@angular/core';
+import { Hero } from '../hero';
 
 describe("HerosComponent", () => {
     let fixture: ComponentFixture<HeroesComponent>;
     let mockHerosService;
     let Heroes;
+
+    // we are going to create another component that is similar to heroes component but it is much simpler. we added this simple component because we need to delete 
+    // this line schemas: [NO_ERRORS_SCHEMA], becuase this line if we have a problem with the html for example we have <adiv> instead of <div> the test will neglect this issue.
+    // so it hide errors in our template.
+    // so we create child mock component
+    @Component({
+        selector: 'app-hero',
+        template: '<div></div>',
+      })
+
+    class FakeHeroComponent {
+        @Input() hero: Hero;
+        //@Output() delete = new EventEmitter();
+      }      
 
     beforeEach(() => {
         // we are going to create special utility called testBed, the testBed allows us to test both the component and
@@ -25,14 +41,14 @@ describe("HerosComponent", () => {
         mockHerosService = jasmine.createSpyObj(['getHeroes', 'addHero', 'deleteHero'])
         
         TestBed.configureTestingModule({
-            declarations: [HeroesComponent],
+            declarations: [FakeHeroComponent, HeroesComponent],
             providers: [
                 // this line means when someone need this service, please use this mock service instead
                 { provide: HeroService, useValue: mockHerosService}
             ],
             // below lines tells angular that for this module do not error if you encounter an unknown attribute or an unknown element in your
             // HTML of the template, just ignore it. mainly this to prevent karma from giving error when it sees routerLink in the html file
-            schemas: [NO_ERRORS_SCHEMA]
+            // schemas: [NO_ERRORS_SCHEMA]
         });
 
         // then we are going to create Test Component. it return fixture component which is a wrapper for a compoent that is 
